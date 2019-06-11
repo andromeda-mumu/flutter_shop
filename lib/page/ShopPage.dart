0ffaa6eba9658ae4a93de65130af3110d020c5d5
wang jiao <1624458215@qq.com>
2019-06-11 19:15:24 +0800

    加载网络数据，但是异步的，数据还没回来时，有异常

--- lib/page/ShopPage.dart
+++ lib/page/ShopPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 
 
 class ShopPage extends StatefulWidget{
@@ -11,15 +13,111 @@ class ShopPage extends StatefulWidget{
 }
 
 class ShopState extends State<ShopPage>{
+  List data;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _pullNet();
+
+  }
+
+  void _pullNet()async{
+    await http.get("http://www.wanandroid.com/project/list/1/json?cid=1")
+        .then((http.Response response){
+      var converDataToJson = json.decode(response.body);
+      converDataToJson = converDataToJson['data']['datas'];
+
+      print(converDataToJson);
+
+      setState(() {
+        data = converDataToJson;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      body: new Scaffold(
-        body: new Center(child: new Text("商铺"),),
-      )
+     body: new ListView(
+//       children: <Widget>[
+//         _getItem2(),
+//         _getItem2()
+//       ],
+     children: _getItem(),
+     ),
     );
+  }
+
+  List<Widget> _getItem(){
+    return data.map((item){
+      return new Card(
+          child: new Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: _getRowWidget(item),
+            ),
+        elevation: 3.0,
+          margin: const EdgeInsets.all(10),
+      );
+    }).toList();
+  }
+
+  Widget _getRowWidget(item){
+    return new Row(children: <Widget>[
+      new Flexible(
+          flex:1,
+          fit:FlexFit.tight,//和android 的weight效果一样
+          child: new Stack(children: <Widget>[
+            new Column(children: <Widget>[
+              new Text('${item['title']}'.trim(),
+                style: new TextStyle(color: Colors.black,fontSize: 20.0,),
+                textAlign: TextAlign.left,
+              ),
+              new Text('${item['desc']}',maxLines: 3,)
+            ],)
+          ],)
+      ),
+      new ClipRect(child: new FadeInImage.assetNetwork(
+        placeholder: 'images/ic_shop_normal.png',
+        image: '${item['envelopePic']}',
+        width: 50,
+        height: 50,
+        fit: BoxFit.fitWidth,
+      ),),
+    ],);
+  }
+
+  Widget _getItem2(){
+    return new Card(
+        child: new Padding(padding: const EdgeInsets.all(10.0),child: _getRowWidget2(),),
+        elevation: 3.0,
+      margin:  const EdgeInsets.all(10.0),
+    );
+  }
+  Widget _getRowWidget2(){
+    return new Row(children: <Widget>[
+      new Flexible(
+          flex:1,
+          fit:FlexFit.tight,//和android 的weight效果一样
+          child: new Stack(children: <Widget>[
+            new Column(children: <Widget>[
+              new Text('title'.trim(),
+                style: new TextStyle(color: Colors.black,fontSize: 20.0,),
+                textAlign: TextAlign.left,
+              ),
+              new Text('desc',maxLines: 3,)
+            ],)
+      ],)
+      ),
+      new ClipRect(child: new FadeInImage.assetNetwork(
+          placeholder: 'images/ic_shop_normal.png',
+          image: 'images/ic_shop_normal.png',
+          width: 50,
+        height: 50,
+        fit: BoxFit.fitWidth,
+      ),),
+    ],);
   }
 
 }
