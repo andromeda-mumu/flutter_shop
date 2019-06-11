6d454fa4495bac6f86354fc59b0046aa9255d3e6
wang jiao <1624458215@qq.com>
2019-06-11 18:20:49 +0800

    StatefulWidget使用context却能跳转时全屏显示

--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/widget/Demo1.dart';
 /**
  * Created by wangjiao on 2019/6/11.
  * description:
  */
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter shop',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       debugShowCheckedModeBanner: false,
       home: new Scaffold(
-        body: new Center(child: new Text('首页'),),
+        body: new Center(child: new RaisedButton(onPressed: _pushPage,child: new Text('跳转'),),),
       ),
     );
   }
+  _pushPage(){
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context){
+      return new Demo1();
+    }));
+  }
 }
+
